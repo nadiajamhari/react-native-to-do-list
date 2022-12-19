d78733116c0c5d653779d988cd2765d7042525bc
Nadia Jamhari <62807626+nadiajamhari@users.noreply.github.com>
2022-12-19 18:31:42 +0800

    draft1: TodoApp list

---
+++ src/components/todo/todo-list.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ITodo from './todo.model';
+
+interface ITodoListProps {
+  data: ITodo[];
+}
+
+const TodoList = (props: ITodoListProps) => {
+  return <></>;
+};
+
+export default TodoList;
